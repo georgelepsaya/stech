dfd499507e0a36372d29bcf2c09dff90d6f64c37
georgelepsaya <georgelepsaya@gmail.com>
2023-06-29 17:32:00 +0300

    changed users index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -33,11 +33,32 @@
             <div class="grid grid-cols-4 gap-5 mt-5">
                 @foreach($users as $user)
                     <div class="p-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg dark:text-gray-200 text-gray-800">
-                        <h4 class="flex flex-row justify-between text-lg font-medium">
-                            <a href="{{ route('users.show', ['id' => $user->id]) }}" class="font-bold {{ ($user->blocked)? 'text-gray-600 dark:text-gray-400' : '' }}">
-                                {{ $user->name }}
-                            </a>
-                        </h4>
+                        <a href="{{route('users.show', ['id' => $user->id])}}" class="flex items-center mb-2">
+                            @if($user->profile_image_path)
+                                <img class="w-16 rounded-3xl mr-3" src="{{ asset('storage/' . $user->profile_image_path) }}" alt="Profile Image">
+                            @else
+                                <img class="w-16 rounded-3xl mr-3" src="{{ asset('storage/images/no-profile.png') }}" alt="No Profile Image">
+                            @endif
+                                <div class="ml-2">
+                                    <h2 class="font-semibold text-xl leading-tight {{ ($user->blocked)? 'text-red-500' : 'text-gray-800 dark:text-gray-200' }}">
+                                        {{ $user->name . (($user->blocked)? ' [blocked]' : '') }}
+                                    </h2>
+                                    <h2 class="font-semibold text-md leading-tight {{ ($user->blocked)? 'text-red-500' : 'text-gray-500 dark:text-gray-200' }}">
+                                        {{ '@' . $user->username }}
+                                    </h2>
+                                </div>
+                        </a>
+                        <div>
+                            <div class="flex items-center my-2 mb-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                                </svg>
+                                <span class="mr-2">Interests</span>
+                            </div>
+                            @foreach($user->tags()->pluck('title')->toArray() as $tag)
+                                <p class="my-2 shadow-sm dark:text-gray-200 dark:bg-gray-600 bg-white px-3 py-1 dark:border-none border border-gray-200 mr-3 rounded-full text-sm">{{$tag}}</p>
+                            @endforeach
+                        </div>
                     </div>
                 @endforeach
             </div>
